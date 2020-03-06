2889f7331ff4726962c0d10b4e5177c653637a67
Tomas <tomadomavicius@gmail.com>
2020-03-06 13:49:15 +0200

    Part 1d - show more / show less functionality

--- client/src/components/TicketListItem.tsx
+++ client/src/components/TicketListItem.tsx
@@ -1,33 +1,94 @@
-import React from 'react'
-import {Ticket} from './../api'
+import React from "react";
+import { Ticket } from "./../api";
+import Truncate from "react-truncate";
+
+const ITEMS_TO_SHOW = 3;
 
 type TicketListItemProps = {
-    key: any;
-	ticket: Ticket;
-	handler: any;
-}
+  key: any;
+  ticket: Ticket;
+  handler: any;
+};
 
-export class TicketListItem extends React.Component<TicketListItemProps> {
-	
-	renderLabels = (labels: string[]) => {
-        return (<ul className='labels'>{labels.map((label) => (<li>{label}</li>))}</ul>);
+type TicketListItemState = {
+  expanded: boolean;
+  truncated: boolean;
+};
+
+export class TicketListItem extends React.Component<TicketListItemProps, TicketListItemState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+      truncated: false
+    };
+
+    this.handleTruncate = this.handleTruncate.bind(this);
+    this.toggleLines = this.toggleLines.bind(this);
+  }
+
+  handleTruncate(truncated: any) {
+    if (this.state.truncated !== truncated) {
+      this.setState({
+        truncated
+      });
     }
+  }
 
-    onHide = () => {
-        this.props.handler(this.props.ticket.id);
-	}
+  toggleLines(event: any) {
+    event.preventDefault();
 
-    public render() {
-        const { ticket } = this.props;
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
 
-        return <li key={ticket.id} className='ticket'>
-            <button className='hide-btn' onClick={this.onHide}>Hide</button>
-            <h5 className='title'>{ticket.title}</h5>
-            <p className='content'>{ticket.content}</p>
-            <footer>
-                <div className='meta-data'>By {ticket.userEmail} | { new Date(ticket.creationTime).toLocaleString()}</div>
-				{ticket.labels ? this.renderLabels(ticket.labels) : null}
-            </footer>
-        </li>
-    }
+  renderLabels = (labels: string[]) => {
+    return (
+      <ul className="labels">
+        {labels.map(label => (
+          <li>{label}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  onHide = () => {
+    this.props.handler(this.props.ticket.id);
+  };
+
+  public render() {
+    const { ticket } = this.props;
+
+    const { expanded, truncated } = this.state;
+
+    return (
+      <li key={ticket.id} className="ticket">
+        <button className="hide-btn" onClick={this.onHide}>
+          Hide
+        </button>
+        <h5 className="title">{ticket.title}</h5>
+        <div className="content">
+          <Truncate
+            lines={!expanded && ITEMS_TO_SHOW}
+            ellipsis={<button onClick={this.toggleLines}>See more</button>}
+            onTruncate={this.handleTruncate}
+          >
+            {ticket.content}
+          </Truncate>
+          {!truncated && expanded && (
+            <button onClick={this.toggleLines}>See less</button>
+          )}
+        </div>
+        <footer>
+          <div className="meta-data">
+            By {ticket.userEmail} |{" "}
+            {new Date(ticket.creationTime).toLocaleString()}
+          </div>
+          {ticket.labels ? this.renderLabels(ticket.labels) : null}
+        </footer>
+      </li>
+    );
+  }
 }
